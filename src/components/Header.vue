219cd309fa9a0c97022eeb4730a7bb531f81e74d
TomasBelfonte <tomas.belfonte@gmail.com>
2022-12-13 17:43:01 +0100

    fix Hover

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        
+    <div class="bg-black"> 
+        <h1 class="text-danger fw-bold text-center py-5" >BoolFlix</h1>
     </div>
 </template>
 
--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -1,24 +1,49 @@
 <template lang="">
     <div class="d-flex">
-        <div class="card my-5 text-center" style="width: 13rem;" >                        
-            <img class="card-img-top" :src="'https://image.tmdb.org/t/p/w342/' + movieDetail.poster_path" alt="Card image cap">
-            <div class="card-body">
-                <p class="card-text">Title: {{movieDetail.title}}</p>
-                <p class="card-text">Original Title: {{movieDetail.original_title}}</p>
-                <p class="card-text">Release date: {{movieDetail.release_date}}</p>
-                <p class="card-text">Original Language: {{movieDetail.original_language}}</p>
-                <p class="card-text">Vote: {{movieDetail.vote_average}}</p>
+        <div class="card my-5 text-center border-danger position-relative" style="width: 13rem;" >                        
+            <img class="tb-card-image card-img-top bg-black" :src=image>
+            <div class="tb-card-detail card-body position-absolute top-0">
+                <p class="card-text text-danger">Title: {{movieDetail.title}}</p>
+                <p class="card-text text-danger">Original Title: {{movieDetail.original_title}}</p>
+                <p class="card-text text-danger">Release date: {{movieDetail.release_date}}</p>
+                <p class="card-text text-danger">Original Language: {{movieDetail.original_language}}</p>
+                <p class="card-text text-danger">Vote: {{movieDetail.vote_average}}</p>
             </div>                    
         </div>
     </div>
+
+    
 </template>
 <script>
     export default {
         props: {
             movieDetail: Object
-        }
+        },
+
+
+        methods: {
+            image() {
+                if (!src === "") {
+                    src="'https://image.tmdb.org/t/p/w342/' + movieDetail.poster_path"
+                } else {
+                    src = '/vite.svg'
+                }
+            },
+
+            image ()
+    }
+
 }
 </script>
 <style scoped>
+
+    .tb-card-image {
+        z-index: 10;
+    }
+    .tb-card-image:hover {
+        opacity: 0 !important;
+    }
+
     
+
 </style>
